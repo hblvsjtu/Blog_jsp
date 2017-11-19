5c3f1923fff124df37b8b8d7192797b598d07b6d
hblvsjtu <hblvsjtu@163.com>
2017-11-19 22:25:51 +0800

    EL语句
    1、学习了EL语句；
    2，学习了request、session、和application的作用于和用法；
    3、修改了登陆注册校验模块的几个bug;
    4、修改了myTitle的bug;

---
+++ Blog_jsp/src/com/lvhongbim/util/StandardTime.java
@@ -0,0 +1,34 @@
+package com.lvhongbim.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class StandardTime {
+	
+	SimpleDateFormat simpleDateFormat=null;
+	Date currentTime=null;
+	private String time;
+	
+
+	public StandardTime() {
+		simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");    
+		currentTime = new Date();    
+		this.time = simpleDateFormat.format(currentTime).toString();
+	}
+	
+	public StandardTime(String newSimpleDateFormat) {
+		simpleDateFormat = new SimpleDateFormat(newSimpleDateFormat);    
+		currentTime = new Date();    
+		this.time = simpleDateFormat.format(currentTime).toString();
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+	
+}
